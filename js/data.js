6f20b462618ab9cb2ad5d29f2060b875b4a367b3
Adekoya Samuel <99253410+Samdek@users.noreply.github.com>
2022-07-17 23:43:48 +0100

    final commit

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const characterData = {
+    hero: {
+        name: "Wizard",
+        avatar: "/img/Orc.jpeg",
+        health: 60,
+        diceCount: 3,
+        currentDiceScore: []
+    },
+    orc: {
+        name: "Orc",
+        avatar: "/img/Orc.jpeg",
+        health: 30,
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "/img/Demon.jpeg",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "/img/Goblin.jpeg",
+        health: 20,
+        diceCount: 3,
+        currentDiceScore: []
+    }
+}
+
+export default characterData
